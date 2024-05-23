0557e5344c65767de518804e55f161cc6cf024ef
bclavie <ben@clavie.eu>
2024-05-23 10:35:46 +0000

    chore: improve cleanup

--- _colbert.py
+++ _colbert.py
@@ -16,67 +16,70 @@ def _make_passage(doc):
 
 
 if __name__ == "__main__":
-    yaml_path, args_list = sys.argv[1], sys.argv[2:]
+    try:
+        yaml_path, args_list = sys.argv[1], sys.argv[2:]
 
-    with open(yaml_path) as f:
-        yaml_cfg = om.OmegaConf.load(f)
+        with open(yaml_path) as f:
+            yaml_cfg = om.OmegaConf.load(f)
 
-    cli_cfg = om.OmegaConf.from_cli(args_list)
-    cfg = om.OmegaConf.merge(yaml_cfg, cli_cfg)
+        cli_cfg = om.OmegaConf.from_cli(args_list)
+        cfg = om.OmegaConf.merge(yaml_cfg, cli_cfg)
 
-    assert isinstance(cfg, om.DictConfig)
+        assert isinstance(cfg, om.DictConfig)
 
-    data_dir = f"{cfg.tmp_dir}/data"
-    huggingface_hub.snapshot_download(repo_id=cfg.train_dataset_id, repo_type="dataset", local_dir=data_dir)
+        data_dir = f"{cfg.tmp_dir}/data"
+        huggingface_hub.snapshot_download(repo_id=cfg.train_dataset_id, repo_type="dataset", local_dir=data_dir)
 
-    if cfg.debug:
-        import srsly
+        if cfg.debug:
+            import srsly
 
-        triplets_path = f"{data_dir}/triples.train.colbert.jsonl"
-        triplets = srsly.read_jsonl(triplets_path)
-        downsampled_triplets = [triplet for i, triplet in enumerate(triplets) if i < 2000]
-        srsly.write_jsonl(triplets_path, downsampled_triplets)
+            triplets_path = f"{data_dir}/triples.train.colbert.jsonl"
+            triplets = srsly.read_jsonl(triplets_path)
+            downsampled_triplets = [triplet for i, triplet in enumerate(triplets) if i < 2000]
+            srsly.write_jsonl(triplets_path, downsampled_triplets)
 
-    model_name = cfg.model_name_or_path.split("/")[-1] if "/" in cfg.model_name_or_path else cfg.model_name_or_path
-    model_name += "_colbert"
+        model_name = cfg.model_name_or_path.split("/")[-1] if "/" in cfg.model_name_or_path else cfg.model_name_or_path
+        model_name += "_colbert"
 
-    train_params = cfg.train_params
-    train_params["root"] = cfg.tmp_dir
-    train_params["name"] = model_name
+        train_params = cfg.train_params
+        train_params["root"] = cfg.tmp_dir
+        train_params["name"] = model_name
 
-    checkpoint = colbert_train(
-        model_name_or_path=cfg.model_name_or_path,
-        train_params=train_params,
-        n_gpu=cfg.n_gpu,
-        data_path=data_dir,
-    )
-
-    for dataset_name in cfg.eval_datasets:
-        int2docid = {}
-        docs = []
-        ds_split = ""
-        dataset = ir_datasets.load(dataset_name)
-
-        for i, doc in enumerate(dataset.docs_iter()):
-            int2docid[i] = doc.doc_id
-            docs.append(_make_passage(doc))
-
-        build_colbert_index(
-            dataset_name=dataset_name,
+        checkpoint = colbert_train(
             model_name_or_path=cfg.model_name_or_path,
-            checkpoint_path=checkpoint,
-            collection=docs,
-            tmp_path=cfg.tmp_dir,
+            train_params=train_params,
+            n_gpu=cfg.n_gpu,
+            data_path=data_dir,
         )
-        score = colbert_score(
-            model_name_or_path=cfg.model_name_or_path,
-            dataset_name=dataset_name,
-            dataset=dataset,
-            int2docid=int2docid,
-            tmp_path=cfg.tmp_dir,
-        )
-        print(f"NDCG@10 for {dataset_name}: {score}")
 
+        for dataset_name in cfg.eval_datasets:
+            int2docid = {}
+            docs = []
+            ds_split = ""
+            dataset = ir_datasets.load(dataset_name)
+
+            for i, doc in enumerate(dataset.docs_iter()):
+                int2docid[i] = doc.doc_id
+                docs.append(_make_passage(doc))
+
+            build_colbert_index(
+                dataset_name=dataset_name,
+                model_name_or_path=cfg.model_name_or_path,
+                checkpoint_path=checkpoint,
+                collection=docs,
+                tmp_path=cfg.tmp_dir,
+            )
+            score = colbert_score(
+                model_name_or_path=cfg.model_name_or_path,
+                dataset_name=dataset_name,
+                dataset=dataset,
+                int2docid=int2docid,
+                tmp_path=cfg.tmp_dir,
+            )
+            print(f"NDCG@10 for {dataset_name}: {score}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
     # Clean up ColBERT artifacts
-    shutil.rmtree("./experiments/default", ignore_errors=True)
-    shutil.rmtree(cfg.tmp_dir, ignore_errors=True)
+        shutil.rmtree("./experiments/default", ignore_errors=True)
+        shutil.rmtree(cfg.tmp_dir, ignore_errors=True)
