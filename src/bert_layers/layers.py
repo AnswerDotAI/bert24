b66d9ca0a6332054d521f4fef52c47a314b919be
Antoine Chaffin <ant54600@hotmail.fr>
2024-05-27 09:35:10 +0000

    Padded version

--- src/bert_layers/layers.py
+++ src/bert_layers/layers.py
@@ -346,6 +346,35 @@ class FlexBertPaddedPreNormLayer(FlexBertLayerBase):
         """
         attn_out = hidden_states + self.attn(self.attn_norm(hidden_states), attn_mask)
         return attn_out + self.mlp(self.mlp_norm(attn_out))
+    
+
+class FlexBertPaddedParallelPreNormLayer(FlexBertLayerBase):
+    """Composes the FlexBERT attention and MLP blocks into a single layer using pre-normalization."""
+
+    def __init__(self, config: FlexBertConfig):
+        super().__init__()
+        self.attn_size = config.hidden_size * 3
+        self.mlp_size = config.intermediate_size
+        # Compute QKV and FF outputs at once
+        self.Wqkvff = nn.Linear(config.hidden_size, self.attn_size + self.mlp_size, bias=config.attn_qkv_bias)
+        self.norm = get_norm_layer(config)
+        self.attn = get_attention_layer(config)
+        self.mlp = get_mlp_layer(config)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attn_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """Forward pass for a BERT layer, including both attention and MLP.
+
+        Args:
+            hidden_states: (total_nnz, dim)
+            attn_mask: None or (batch, max_seqlen)
+        """
+        # Compute QKV and FF outputs at once and split them
+        qkv, intermediate_ff = self.Wqkvff(self.norm(hidden_states)).split([self.attn_size, self.mlp_size], dim=2)
+        return hidden_states + self.attn(qkv, attn_mask) + self.mlp(intermediate_ff)
 
 
 class FlexBertUnpadPostNormLayer(FlexBertLayerBase):
@@ -409,6 +438,7 @@ LAYER2CLS = {
     "unpadded_parallel_prenorm": FlexBertUnpadParallelPreNormLayer,
     "unpadded_postnorm": FlexBertUnpadPostNormLayer,
     "padded_prenorm": FlexBertPaddedPreNormLayer,
+    "padded_parallel_prenorm": FlexBertPaddedParallelPreNormLayer,
     "padded_postnorm": FlexBertPaddedPostNormLayer,
 }
 
