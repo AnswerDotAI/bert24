0cecf704ae5e67afc7fc2a3e813f54f109752a07
Oskar Hallström <79756299+ohallstrom@users.noreply.github.com>
2024-06-04 17:02:42 +0200

    Correct norm placement when using pre layer norm attention (#50)
    * Add option for final norm in config
    ** Must be used with prenorm attention, should not be with post norm attention
    
    * Update embedding norm for prenorm attention
    ** Deal with embedding norm as llama and falcon as default
    ** Allow CrammingBERT-style double norm
    
    * Have assert for bert_layer being either postnorm or prenorm
    
    * Fix adamw info string

--- src/flex_bert.py
+++ src/flex_bert.py
@@ -106,6 +106,12 @@ def create_flex_bert_mlm(
 
     config = configuration_bert_module.FlexBertConfig.from_pretrained(pretrained_model_name, **model_config)
 
+    if "prenorm" in config.bert_layer:
+        assert config.final_norm, "Final norm must be used with prenorm attention"
+    else:
+        assert "postnorm" in config.bert_layer, "config.bert_layer str must contain either prenorm or postnorm"
+        assert not config.final_norm, "Final norm should not be used with postnorm attention"
+
     # Padding for divisibility by 8
     if config.vocab_size % 8 != 0:
         config.vocab_size += 8 - (config.vocab_size % 8)
