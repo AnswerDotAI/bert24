Fix to respect ignore_token arg when masking
Co-authored-by: fladhak <fladhak@users.noreply.github.com>
Co-authored-by: Benjamin Warner <warner-benjamin@users.noreply.github.com>

src/sequence_packer.py
@@ -293,7 +293,7 @@ class SequencePacker(ABC):
                 labels: the original input seq with non-masked tokens replaced by `ignore_index`
         """
         # Create labels
-        labels = np.where(seq == pad_token, -100, seq)
+        labels = np.where(seq == pad_token, ignore_index, seq)
 
         # Create a single mask
         rand = np_rng.random(seq.shape)