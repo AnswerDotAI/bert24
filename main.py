0fac7f5033c6a19b17326ec331de7f85d3c8ce04
Benjamin Warner <me@benjaminwarner.dev>
2024-09-26 22:26:27 -0400

    add restart override flag to allow changing schedule, callbacks, and microbatch_size

--- main.py
+++ main.py
@@ -451,7 +451,14 @@ def main(cfg: DictConfig, return_trainer: bool = False, do_train: bool = True) -
 
     if do_train:
         print("Starting training...")
-        trainer.fit()
+        if cfg.get("restart_override", False):
+            trainer.fit(
+                schedulers=scheduler,
+                callbacks=callbacks,
+                device_train_microbatch_size=cfg.get("device_train_microbatch_size", "auto"),
+            )
+        else:
+            trainer.fit()
 
     if return_trainer:
         return trainer
