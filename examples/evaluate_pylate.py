fdb1e1cce507962f63336ab37e1483cbe96da78c
Antoine Chaffin <ant54600@hotmail.fr>
2024-11-25 10:09:29 +0000

    Cleaning up pylate evaluation

--- examples/evaluate_pylate.py
+++ examples/evaluate_pylate.py
@@ -1,36 +1,7 @@
-import argparse
-
-# Parse command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--models", type=str, nargs="+", required=True, help="Models to use")
-parser.add_argument("--lrs", type=str, nargs="+", default=["8e-5"], help="Learning rates to use")
-parser.add_argument("--datasets", type=str, nargs="+", required=True, help="Datasets to use")
-args = parser.parse_args()
-
-
 from collections import defaultdict
-from typing import Literal
 
 import srsly
 from pylate import evaluation, indexes, models, retrieve
-
-VALID_DATASETS = ["mldr", "trec-covid", "fiqa", "scifact", "nfcorpus"]
-
-model_names = args.models
-lrs = [float(x) for x in args.lrs]
-if args.datasets[0] == "all":
-    eval_datasets = VALID_DATASETS
-elif args.datasets[0] == "beir":
-    eval_datasets = ["trec-covid", "fiqa", "scifact", "nfcorpus"]
-elif args.datasets[0] == "nanobeir":
-    raise NotImplementedError
-elif args.datasets[0] == "long_context":
-    eval_datasets = ["mldr"]
-else:
-    assert all([x in VALID_DATASETS for x in args.datasets])
-    eval_datasets = args.datasets
-
-ALL_RESULTS = defaultdict(lambda: defaultdict(dict))
 
 
 def fetch_most_recent_checkpoint(model_path: str):
@@ -50,83 +21,60 @@ def fetch_most_recent_checkpoint(model_path: str):
     return str(latest_checkpoint)
 
 
-def eval_model(model_path: str, dataset: Literal["scifact", "nfcorpus"]):
-    document_length = 8190
-    if (
-        any(x in model_path.lower() for x in ["deberta", "bert-base", "bert-large", "roberta"])
-        and "mldr" not in dataset
-    ):
-        document_length = 510
-    model = models.ColBERT(
-        model_name_or_path=fetch_most_recent_checkpoint(model_path),
-        document_length=document_length,
-        trust_remote_code=True,
-    )
+eval_datasets = ["fiqa", "scifact", "nfcorpus", "trec-covid"]
+model_name = "ModernBERT/bert24-base-v2-2ep-decay_100B-0.08-lr"
+model_shortname = model_name.split("/")[-1]
+lr = 8e-5
+model_results = defaultdict(dict)
+run_name = f"{model_shortname}-colbert-KD-{lr}"
+output_dir = f"output/{model_shortname}/{run_name}"
+model = models.ColBERT(
+    model_name_or_path=fetch_most_recent_checkpoint(output_dir),
+    document_length=510,
+    trust_remote_code=True,
+)
+for dataset in eval_datasets:
+    for eval_dataset in eval_datasets:
+        index = indexes.Voyager(index_name=dataset, override=True, M=200, ef_construction=500, ef_search=500)
 
-    index = indexes.Voyager(
-        index_name=f"new_evals_gpu{args.gpu}", override=True, M=200, ef_construction=500, ef_search=500
-    )
+        retriever = retrieve.ColBERT(index=index)
 
-    retriever = retrieve.ColBERT(index=index)
-
-    # Download the SciFact dataset
-    if "mldr" in dataset:
-        documents, queries, qrels = evaluation.load_custom_dataset(
-            path="/opt/home/bert24/pylate/data/mldr_subsample_15k",
-            split="dev",
-        )
-    else:
         documents, queries, qrels = evaluation.load_beir(
             dataset_name=dataset,
             split="test",
         )
 
-    batch_size = 500
+        batch_size = 500
 
-    documents_embeddings = model.encode(
-        sentences=[document["text"] for document in documents],
-        batch_size=batch_size,
-        is_query=False,
-        show_progress_bar=True,
-    )
+        documents_embeddings = model.encode(
+            sentences=[document["text"] for document in documents],
+            batch_size=batch_size,
+            is_query=False,
+            show_progress_bar=True,
+        )
 
-    index.add_documents(
-        documents_ids=[document["id"] for document in documents],
-        documents_embeddings=documents_embeddings,
-    )
+        index.add_documents(
+            documents_ids=[document["id"] for document in documents],
+            documents_embeddings=documents_embeddings,
+        )
 
-    queries_embeddings = model.encode(
-        sentences=queries,
-        is_query=True,
-        show_progress_bar=True,
-        batch_size=16,
-    )
+        queries_embeddings = model.encode(
+            sentences=queries,
+            is_query=True,
+            show_progress_bar=True,
+            batch_size=16,
+        )
 
-    scores = retriever.retrieve(queries_embeddings=queries_embeddings, k=20, k_token=80, batch_size=2)
+        scores = retriever.retrieve(queries_embeddings=queries_embeddings, k=20, k_token=80, batch_size=2)
 
-    evaluation_scores = evaluation.evaluate(
-        scores=scores,
-        qrels=qrels,
-        queries=queries,
-        metrics=["ndcg@10"],
-    )
-    return evaluation_scores
-
-
-if __name__ == "__main__":
-    for model_name in model_names:
-        model_results = defaultdict(dict)
-        model_shortname = model_name.split("/")[-1]
-        for lr in lrs:
-            # Set the run name for logging and output directory
-            run_name = f"{model_shortname}-colbert-KD-{lr}"
-            output_dir = f"output/{model_shortname}/{run_name}"
-
-            for eval_dataset in eval_datasets:
-                results = eval_model(output_dir, eval_dataset)
-                print(f"{model_name} - {lr} - {eval_dataset}")
-                print(results)
-                print("-----------")
-                model_results[lr][eval_dataset] = results
-                ALL_RESULTS[model_name][lr][eval_dataset] = results
-        srsly.write_json(f"output/{model_shortname}/{model_shortname}_results.json", model_results)
+        evaluation_scores = evaluation.evaluate(
+            scores=scores,
+            qrels=qrels,
+            queries=queries,
+            metrics=["ndcg@10"],
+        )
+        print(f"{model_name} - {lr} - {eval_dataset}")
+        print(evaluation_scores)
+        print("-----------")
+        model_results[eval_dataset] = evaluation_scores
+    srsly.write_json(f"output/{model_shortname}/{model_shortname}_results.json", model_results)
